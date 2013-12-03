c673cc7bac4dc7555126e8d803801f70edf66121
samsara091 <allabhill@gmail.com>
2013-12-03 15:48:53 -0800

    Finish layout and routes

--- Gemfile
+++ Gemfile
@@ -22,6 +22,7 @@ gem 'coffee-rails', '~> 4.0.0'
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
 gem 'capybara', '1.1.2'
+gem "bootstrap-sass", "~> 2.1.0.0"
 
 # Turbolinks makes following links in your web application faster. Read more: https://github.com/rails/turbolinks
 gem 'turbolinks'
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,46 +1,90 @@
 require 'spec_helper'
 
 describe "StaticPages" do
-  # describe "GET /static_pages" do
+
+  subject { page }
+
   describe "Home page" do
-    it "should have the h1 content 'Web App'" do
-      visit '/static_pages/home'
-      page.should have_selector('h1', :text => 'Web App')
-      # get static_pages_index_path
-      # response.status.should be(200)
-    end
-    it "should have the base title" do
-      visit '/static_pages/home'
-      page.should have_selector('title',
-               		 :text => "Ruby on Rails Tutorial Web App")
-    end
-    it "should not have a custom page title" do
-      visit '/static_pages/home'
-      page.should_not have_selector('title', :text => '| Home')
-    end
+    before { visit root_path }
+
+    it { should have_content('Web App') }
+    it { should have_selector('title',
+                   text: " ") }
+    it { should_not have_selector('title',
+                   text: "Ruby on Rails Tutorial Web App | Home") }
   end
+
   describe "Help page" do
+    before { visit help_path }
 
-    it "should have the content 'Help'" do
-      visit '/static_pages/help'
-      page.should have_content('Help')
-    end
-    it "should have the title 'Help'" do
-      visit '/static_pages/help'
-      page.should have_selector('title',
-                        :text => "Ruby on Rails Tutorial Web App | Help")
-    end
+    it { should have_content('Help') }
+    it { should have_selector('title',
+                   text: "Ruby on Rails Tutorial Web App | Help") }
   end
-  describe "About page" do
 
-    it "should have the content 'About Us'" do
-      visit '/static_pages/about'
-      page.should have_content('About Us')
-    end
-    it "should have the title 'About Us'" do
-      visit '/static_pages/about'
-      page.should have_selector('title',
-                    :text => "Ruby on Rails Tutorial Web App | About Us")
-    end
+  describe "About page" do
+    before { visit about_path }
+
+    it { should have_content('About') }
+    it { should have_selector('title',
+                   text: "Ruby on Rails Tutorial Web App | About") }
+  end
+
+  describe "Contact page" do
+    before { visit contact_path }
+
+    it { should have_content('Contact') }
+    it { should have_selector('title',
+                   text: "Ruby on Rails Tutorial Web App | Contact") }
   end
 end
+
+# describe "StaticPages" do
+#   # describe "GET /static_pages" do
+#   subject { page }
+#   describe "Home page" do
+#   	before { visit root_path }
+#     it {should have_content ('Web App') }
+#       # get static_pages_index_path
+#       # response.status.should be(200)
+#     it {should have_title("Ruby on Rails Tutorial Web App")
+#     it {should_not have_title('| Home')
+#     end
+#   describe "Help page" do
+
+#     it "should have the content 'Help'" do
+#       visit help_path
+#       page.should have_content('Help')
+#     end
+#     it "should have the title 'Help'" do
+#       visit help_path
+#       page.should have_selector('title',
+#                         :text => "Ruby on Rails Tutorial Web App | Help")
+#     end
+#   end
+#   describe "About page" do
+
+#     it "should have the content 'About Us'" do
+#       visit about_path
+#       page.should have_content('About Us')
+#     end
+#     it "should have the title 'About Us'" do
+#       visit about_path
+#       page.should have_selector('title',
+#                     :text => "Ruby on Rails Tutorial Web App | About Us")
+#     end
+#   end
+#   describe "Contact page" do
+
+#     it "should have the h1 'Contact'" do
+#       visit contact_path
+#       page.should have_selector('h1', text: 'Contact')
+#     end
+
+#     it "should have the title 'Contact'" do
+#       visit contact_path
+#       page.should have_selector('title',
+#                     text: "Ruby on Rails Tutorial Web App | Contact")
+#     end
+#   end
+# end
